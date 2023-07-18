91748204e524a39ce0c61d69c3b0db4fdc35f756
zanedewar <zane_dewar@hotmail.com>
2023-07-18 04:45:13 -0700

    Graphics
    Graphics Test

--- bball.py
+++ bball.py
@@ -2,6 +2,7 @@ import tkinter
 import pybaseball
 import pandas
 import rosters
+from tkinter import ttk
 rostDic = {}
 class GUI:
     def __init__(self, first, last):
@@ -17,7 +18,20 @@ class GUI:
                         'orioles': self.roster.orioles,'padres': self.roster.padres,'phillies': self.roster.phillies,'pirates': self.roster.pirates,'rangers':self.roster.rangers,
                         'rays': self.roster.rays,'redsox':self.roster.redsox,'reds': self.roster.reds,'rockies': self.roster.rockies,'royals': self.roster.royals,
                         'tigers':self.roster.tigers,'twins':self.roster.twins,'whitesox': self.roster.whitesox,'yankees': self.roster.yankees}
+        self.currTeam = 'Mariners'
+        self.initGraphics()
         
     def getID(first, last):
         return pybaseball.playerid_lookup(last,first,fuzzy=True).key_bbref
+    def initGraphics(self):
+        teamVar = tkinter.StringVar()
+        playerVar = tkinter.StringVar()
+        team = ttk.Combobox(self.g, textvariable = teamVar)
+        player = ttk.Combobox(self.g, textvariable = playerVar)
+        team['values'] = rosters.baseball
+        player['values'] = self.rostDic[self.currTeam.lower()]
+        team.pack()
+        player.pack()
+        self.g.mainloop()
+        #hello a
 print("hello")
